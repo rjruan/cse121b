e5838b650b70914a370263e9439299dd3be424be
rjruan <rua22001@byui.edu>
2024-01-20 18:54:19 +0800

    week2test

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,23 @@
+/* Step 2 - Variables */
+let fullName = 'Ruby Ruan';
+let currentYear = '2023';
+const profilePicture = 'images/ruby copy.jpg';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('profileImage');
+const foodElement = document.getElementById('food'); // Added this line to get the food element
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+imageElement.setAttribute('src', profilePicture);
+
+/* Step 5 - Array */
+let favFoods = ['Stinky Tofu', 'Beef Noodle', 'HotPot'];
+
+// Modify the HTML element with the id of 'food' to display your favorite foods array
+foodElement.innerHTML = favFoods.join('<br>');
+
+
+imageElement.setAttribute('src', profilePicture);
