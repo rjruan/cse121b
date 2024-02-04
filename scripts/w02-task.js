aa3d9f753285179673381e03d43fb8b61095cd0d
rjruan <rua22001@byui.edu>
2024-02-04 13:39:58 +0800

    w04

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,7 +1,7 @@
 /* Step 2 - Variables */
 let fullName = 'Ruby Ruan';
 let currentYear = '2023';
-const profilePicture = 'images/ruby copy.jpg';
+const profilePicture = 'images/9421.jpg';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
---
+++ scripts/w04-task.js
@@ -0,0 +1,82 @@
+/* Profile Object */
+/* Profile Object */
+let myProfile = {
+    name: "Ruby Ruan",
+    photo: 'images/9421.jpg',
+    favoriteFoods: [
+        'Stinky Tofu',
+        'Chocolate',
+        'HotPot',
+    ],
+    hobbies: [
+        'Swimming',
+        'Listening to music',
+        'Eating food',
+    ],
+    placesLived: [
+        {
+            place: 'Miaoli, Taiwan',
+            length: '18 years'
+        },
+        {
+            place: 'Taipei, Taiwan',
+            length: '1 year'
+        },
+        {
+            place: 'Pingdong, Taiwan',
+            length: '3 months'
+        },
+        {
+            place: 'Taichung, Taiwan',
+            length: '8 months'
+        },
+        {
+            place: 'Kaohsiung, Taiwan',
+            length: '3 months'
+        },
+        {
+            place: 'Yunlin, Taiwan',
+            length: '3 months'
+        },
+        {
+            place: 'Rexburg, Idaho',
+            length: '2 years'
+        },
+    ]
+};
+/* DOM Manipulation - Output */
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Photo with attributes */
+const photoElement = document.querySelector('#photo');
+photoElement.setAttribute('src', myProfile.photo);
+photoElement.setAttribute('alt', myProfile.name);
+
+/* Favorite Foods List */
+const favoriteFoodsList = document.querySelector('#favorite-foods');
+myProfile.favoriteFoods.forEach(food => {
+    const liElement = document.createElement('li');
+    liElement.textContent = food;
+    favoriteFoodsList.appendChild(liElement);
+});
+
+/* Hobbies List */
+const hobbiesList = document.querySelector('#hobbies');
+myProfile.hobbies.forEach(hobby => {
+    const liElement = document.createElement('li');
+    liElement.textContent = hobby;
+    hobbiesList.appendChild(liElement);
+});
+
+/* Places Lived DataList */
+const placesLivedDl = document.querySelector('#places-lived');
+myProfile.placesLived.forEach(place => {
+    const dtElement = document.createElement('dt');
+    dtElement.textContent = place.place;
+
+    const ddElement = document.createElement('dd');
+    ddElement.textContent = place.length;
+
+    placesLivedDl.appendChild(dtElement);
+    placesLivedDl.appendChild(ddElement);
+});
